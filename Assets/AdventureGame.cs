ef7412732f674ab835333b0ece4cbfc5645f0be3
ke4tri <ke4tri@gmail.com>
2019-08-18 18:44:45 -0500

    idk

---
+++ Assets/AdventureGame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    // Instance Variable
+    [SerializeField] Text textComponent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameText();  
+    }
+
+    void GameText()
+    {
+        textComponent.text = ("This is going to be the game text");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
