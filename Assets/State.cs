423ae7808c1f1c6b6c90c2cfe9435a5f319c3896
ke4tri <ke4tri@gmail.com>
2019-08-18 19:12:56 -0500

    starting state

--- Assets/State.cs
+++ Assets/State.cs
@@ -2,17 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class State : MonoBehaviour
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   [TextArea(10,14)] [SerializeField] string storyText;
+
 }
