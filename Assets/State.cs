f2f808b7f76c0f5ed92f7748641b5113e4b71d6b
ke4tri <ke4tri@gmail.com>
2019-08-21 20:56:47 -0500

    Using array for state, created two rooms that render with respective keypress

--- Assets/State.cs
+++ Assets/State.cs
@@ -8,8 +8,19 @@ public class State : ScriptableObject
 
    [TextArea(10,14)] [SerializeField] string storyText;
 
+   [SerializeField] State[] nextStates;
+  
+
+
+
     public string GetStateStory() {
         return storyText;
     }
 
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+
+  
 }
